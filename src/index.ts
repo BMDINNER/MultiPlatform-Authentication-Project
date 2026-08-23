957b97cf3cfd53abcfe4d7473d6f32690496d30d
BerkeDinner <berkemstdinner@outlook.com>
2026-08-23 07:01:19 -0700

    small update

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,13 @@
 import app from './server.js';
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
-  });
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 const PORT = process.env.PORT || 3001;
 
